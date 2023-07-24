0957cdb0a483c9f7a4fe4075f34a4f4ef51525ff
Semiranov <semiranov@gmail.com>
2023-07-24 12:39:28 +0500

    hw2

---
+++ src/HW2/Hw2.java
@@ -0,0 +1,7 @@
+package HW2;
+
+public class Hw2 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/sm2/Main.java
+++ src/sm2/Main.java
@@ -1,2 +1,43 @@
-package sm2;public class Main {
+package sm2;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        File myFile = new File("/Users/vladimirsemiranov/Work/Учеба/Exp/Exception/src/sm2/name.txt");
+        HashMap<String, String> myHashMap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
+            String stringName = new String();
+            while ((stringName = reader.readLine()) != null) {
+                String[] str = stringName.split("=");
+                if (str[1].equals("?")) {
+                    str[1] = String.valueOf(str[0].length());
+                }
+                myHashMap.put(str[0], str[1]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        writeFile("/Users/vladimirsemiranov/Work/Учеба/Exp/Exception/src/sm2/name.txt", myHashMap);
+    }
+
+    public static void writeFile(String path, HashMap myHashMap) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            myHashMap.forEach((key, value) -> {
+                String rezult = key + "=" + value;
+                try {
+                    writer.write(rezult + "\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+//                System.out.println(value);
+            });
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
+
