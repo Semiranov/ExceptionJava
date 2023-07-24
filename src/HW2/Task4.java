d949e7317f67d95e9f59fb8d9f00f59bded8bb4f
Semiranov <semiranov@gmail.com>
2023-07-24 14:34:49 +0500

    Task4

---
+++ src/HW2/Task4.java
@@ -0,0 +1,27 @@
+package HW2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//Разработайте программу, которая выбросит Exception,
+//когда пользователь вводит пустую строку.
+//Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+public class Task4 {
+    public static void main(String[] args) {
+        emptyString();
+    }
+
+    private static void emptyString() {
+        System.out.println("Введите текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        if (string.equals("")) {
+            throw new RuntimeException("Пустые строки вводить нельзя!");
+
+        }
+        System.out.println("Вы ввели: " + string);
+        scanner.close();
+    }
+}
