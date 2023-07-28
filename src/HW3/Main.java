99ba7c2629c88c9809d3f665dfb66bebbb2c5c30
Semiranov <semiranov@gmail.com>
2023-07-28 19:38:20 +0500

    HW3

---
+++ src/HW3/Main.java
@@ -0,0 +1,11 @@
+package HW3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args)  {
+        Contact contact = View.getUserInput();
+        Model.saveContact(contact);
+    }
+}
---
+++ src/sm3/Counter.java
@@ -0,0 +1,4 @@
+package sm3;
+
+public class Counter {
+}
